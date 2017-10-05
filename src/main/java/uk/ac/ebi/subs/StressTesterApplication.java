07a07015eebbc70260930fc90869ff4ed65aeec0
Neil Goodgame <neilg@ebi.ac.uk>
2017-10-05 12:33:48 +0100

    Disable mongo auto configuration

--- src/main/java/uk/ac/ebi/subs/StressTesterApplication.java
+++ src/main/java/uk/ac/ebi/subs/StressTesterApplication.java
@@ -6,13 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import uk.ac.ebi.subs.stresstest.StressTestService;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-@SpringBootApplication
 
+@SpringBootConfiguration
+@EnableAutoConfiguration(exclude={MongoRepositoriesAutoConfiguration.class,MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+        @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
 public class StressTesterApplication implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(StressTesterApplication.class);
