53a63ee3ea061fbdcba54e58416334e7b716b311
Neil Goodgame <neilg@ebi.ac.uk>
2018-03-06 13:27:51 +0000

    Passes in RestOperations so that the AAP token is available in the header

--- src/main/java/uk/ac/ebi/subs/stresstest/ApiLinkDiscovery.java
+++ src/main/java/uk/ac/ebi/subs/stresstest/ApiLinkDiscovery.java
@@ -3,17 +3,17 @@ package uk.ac.ebi.subs.stresstest;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.client.Traverson;
+import org.springframework.web.client.RestOperations;
 
 import java.net.URI;
 
 public class ApiLinkDiscovery {
 
 
-    public Link discoverNamedLink(URI uri, String... linkNames){
+    public Link discoverNamedLink(RestOperations restOperations, URI uri, String... linkNames){
         Traverson traverson = new Traverson(uri, MediaTypes.HAL_JSON);
-
+        traverson.setRestOperations(restOperations);
         Traverson.TraversalBuilder builder = traverson.follow(linkNames);
-
         return builder.asLink();
     }
 }
